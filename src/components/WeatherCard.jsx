a14d33baf38359c071e00d3453d2964722cc7004
Chi Thanh Le <chi.thanh.le1973@gmail.com>
2026-08-17 16:50:26 +0200

    refactor: extract SearchBar component from WeatherCard.jsx

--- src/components/WeatherCard.jsx
+++ src/components/WeatherCard.jsx
@@ -1,6 +1,5 @@
 import {useState} from 'react';
-import {getLocationCoordinates} from '../utils/getLocationCoordinates';
-import {getWeatherData} from '../utils/getWeatherData';
+import {SearchBar} from './SearchBar';
 
 export function WeatherCard({inputRef, setIsDay}) {
   const [city, setCity] = useState('');
@@ -8,48 +7,16 @@ export function WeatherCard({inputRef, setIsDay}) {
   const [error, setError] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
 
-  async function handleSearch() {
-    setIsLoading(true);
-    setError(false);
-
-    const cityName = inputRef.current.value;
-
-    const response = await getLocationCoordinates(cityName);
-    if (!response) {
-      setError(true);
-      setIsLoading(false);
-      return;
-    }
-    setCity(response.name);
-
-    const weatherData = await getWeatherData(
-      response.latitude,
-      response.longitude,
-    );
-    setIsDay(weatherData.data.current_weather.is_day);
-    setWeather(weatherData);
-    setIsLoading(false);
-    console.log(weatherData);
-  }
-
   return (
     <main className="m-auto max-w-lg h-96 backdrop-blur-xl border-solid border-2 mt-8 p-8 rounded-4xl sm:mx-auto">
-      <section>
-        <div className="relative">
-          <input
-            className="w-full border-solid border-2 rounded-full bg-slate-50 min-h-12 p-2"
-            ref={inputRef}
-            type="text"
-          />
-          <button
-            onClick={handleSearch}
-            className="absolute right-1 bg-blue-500 top-1/2 -translate-y-1/2 rounded-full w-10 sm:w-24 h-10 flex justify-center items-center gap-1 text-slate-50 transition-[width] cursor-pointer"
-          >
-            <span className="material-symbols-outlined">search</span>
-            <p className="hidden sm:block">Search</p>
-          </button>
-        </div>
-      </section>
+      <SearchBar
+        inputRef={inputRef}
+        setCity={setCity}
+        setWeather={setWeather}
+        setError={setError}
+        setIsLoading={setIsLoading}
+        setIsDay={setIsDay}
+      />
 
       <section className="flex flex-col items-center text-3xl mt-4">
         {isLoading ? (
