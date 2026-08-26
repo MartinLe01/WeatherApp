4a02916afd2a78178df6bd333e5a3b6fee4df87c
Chi Thanh Le <chi.thanh.le1973@gmail.com>
2026-08-26 20:44:00 +0200

    feat: display full 24 hour hourly forecast

--- src/utils/getWeatherData.js
+++ src/utils/getWeatherData.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export async function getWeatherData(latitude, longitude) {
   const response = await axios.get(
-    `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current_weather=true&timezone=auto&hourly=temperature_2m,weather_code&forecast_days=1`,
+    `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current_weather=true&timezone=auto&hourly=temperature_2m,weather_code&forecast_days=2`,
   );
 
   return response;
